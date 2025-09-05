8e0369d293ae38dc044e3ca89e02e3a2c4c41d4e
Vladyslav <vladurlov17@gmail.com>
2025-09-05 12:31:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+    
+        Console.Write("Введи перше число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введи дію (+ - * /): ");
+        string op = Console.ReadLine();
+
+        Console.Write("Введи друге число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        switch (op)
+        {
+            case "+":
+                Console.WriteLine("Результат: " + (a + b));
+                break;
+            case "-":
+                Console.WriteLine("Результат: " + (a - b));
+                break;
+            case "*":
+                Console.WriteLine("Результат: " + (a * b));
+                break;
+            case "/":
+                Console.WriteLine("Результат: " + (a / b));
+                break;
+            default:
+                Console.WriteLine("Невірна дія");
+                break;
+        }
